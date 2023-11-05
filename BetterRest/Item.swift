048edd0cb1416a1ed1cc3d95e2b06f3610da7d02
Yusuf <ybkocaman@gmail.com>
2023-11-05 20:35:30 +0000

    Initial Commit

---
+++ BetterRest/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  BetterRest
+//
+//  Created by Yusuf Burak on 05/11/2023.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
